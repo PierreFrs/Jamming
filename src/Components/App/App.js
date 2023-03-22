41e67ecdb50cb2ac061c546ece6b29a4909d21fd
PierreFrs <pierrefraisse90@gmail.com>
2023-03-22 16:08:17 +0100

    css corrections

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -12,18 +12,33 @@ class App extends React.Component {
     super(props);
     this.state = {
       searchResults: [
-        {name: 'name1',
+        { name: 'name1',
         artist: 'artist1',
         album: 'album1',
-        id: 'id1'}, 
-        {name: 'name2',
+        id: 1 }, 
+        { name: 'name2',
         artist: 'artist2',
         album: 'album2',
-        id: 'id2'}, 
-        {name: 'name3',
+        id: 2 }, 
+        { name: 'name3',
         artist: 'artist3',
         album: 'album3',
-        id: 'id3'}, 
+        id: 3 }, 
+      ],
+      playlistName: 'cool music',
+      playlistTracks: [
+        { name: 'playlistName1',
+        artist: 'playlistArtist1',
+        album: 'playlistAlbum1',
+        id: 4 }, 
+        { name: 'playlistName',
+        artist: 'playlistArtist2',
+        album: 'playlistAlbum2',
+        id: 5 }, 
+        { name: 'playlistName3',
+        artist: 'playlistArtist3',
+        album: 'playlistAlbum3',
+        id: 6 }
       ]
     }
   }
@@ -35,7 +50,8 @@ class App extends React.Component {
           <SearchBar />
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults}/>
-            <Playlist />
+            <Playlist playlistName={this.state.playlistName} 
+            playlistTracks={this.state.playlistTracks}/>
           </div>
         </div>
       </div>
